27cb0887789c70927a56e41ff1b40d3e419caa56
Connor Cowan <ccowan003@comcast.net>
2019-08-07 20:36:51 +0000

    notes

--- lib/box_office/movie.rb
+++ lib/box_office/movie.rb
@@ -9,3 +9,19 @@ class Movie
     
   end
 end
+
+#NOTES Was not working quite properly
+
+
+  def sort_grosstotal
+    BoxOffice::Movie.all.map( &:to_s ).sort {|a,b| b.grosstotal <=> a.grosstotal}.each.with_index(1) do |movie, index|
+      puts "#{movie.title}"
+    end
+  end
+  
+  def sort_numberweeks
+    BoxOffice::Movie.all.reverse.sort_by {|key, value| value
+    puts "#{key.title} #{key.numberweeks}"
+    }
+  end
+
